6b9472e4a53e9e4104bbe52a5656cd5dd55a1fad
Nick Springham <Nspringham@aol.co.uk>
2018-09-29 16:04:24 +0100

    added number display functionality

--- app.js
+++ app.js
@@ -14,14 +14,38 @@ var nineBtn = document.getElementById('calc-nine');
 var zeroBtn = document.getElementById('calc-zero');
 
 // calc operators
-var clearBtn = document.getElementById('calc-clear');
-var backspaceBtn = document.getElementById('calc-backspace');
 var divideBtn = document.getElementById('calc-divide');
 var multiplyBtn = document.getElementById('calc-multiply');
 var subtractBtn = document.getElementById('calc-subtract');
 var addBtn = document.getElementById('calc-add');
+
+// calc functions
+var clearBtn = document.getElementById('calc-clear');
+var backspaceBtn = document.getElementById('calc-backspace');
 var decimalBtn = document.getElementById('calc-decimal');
 var calculateBtn = document.getElementById('calc-calculate');
 
 // calc display
 var displayValElement = document.getElementById('display-val');
+var displayVal = '0';
+var pendingVal;
+var evalStringArray = [];
+
+var calcNumBtns = document.getElementsByClassName('calc-num');
+var calcOperatorBtns = document.getElementsByClassName('calc-operator');
+
+var updateDisplayVal = (clickObj) => {
+  var btnText = clickObj.target.innerText;
+  if (displayVal === '0') 
+    displayVal = '';
+
+  displayVal += btnText
+  displayValElement.innerText = displayVal;
+}
+
+for (let i = 0; i < calcNumBtns.length; i++) {
+  calcNumBtns[i].addEventListener('click', updateDisplayVal, false);
+}
+// for (let i = 0; i < calcOperatorBtns.length; i++) {
+//   calcOperatorBtns[i].addEventListener('click', performOperation, false);
+// }
